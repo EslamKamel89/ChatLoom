66f5d99ebb59c396ffee160ab069608a8514cb30
EslamKamel89 <elsamkamelforex@gmail.com>
2025-04-03 12:16:48 +0200

    Update message input in Show page footer; replace input with textarea for better user experience

--- resources/js/pages/chat/room/Show.vue
+++ resources/js/pages/chat/room/Show.vue
@@ -43,13 +43,13 @@ defineProps<{
                 <!-- END Page Content -->
 
                 <!-- Page Footer -->
-                <footer class="fixed bottom-0 items-center border-t border-slate-200/75 bg-white lg:start-80">
-                    <form class="container mx-auto flex h-20 items-center gap-2 px-4 lg:px-8 xl:max-w-7xl" onsubmit="return false;">
-                        <input
+                <footer class="fixed bottom-0 mb-3 w-full items-center border-t border-slate-200/75 bg-white px-3 lg:w-[75%]">
+                    <form class="container mx-auto flex h-20 items-center gap-2 px-4" onsubmit="return false;">
+                        <textarea
                             type="text"
                             class="-mx-5 block w-full rounded-lg border-0 px-5 py-4 leading-6 focus:border-indigo-500 focus:ring focus:ring-indigo-500/75"
                             placeholder="Type a new message and hit enter.."
-                        />
+                        ></textarea>
                     </form>
                 </footer>
                 <!-- END Page Footer -->
