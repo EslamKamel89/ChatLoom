af53775581236ce947b847eef0c946f808e598b7
EslamKamel89 <elsamkamelforex@gmail.com>
2025-04-03 13:00:35 +0200

    Add ChatFooter component for message input in chat room

--- resources/js/pages/chat/room/Show.vue
+++ resources/js/pages/chat/room/Show.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import ChatFooter from '@/components/chat/ChatFooter.vue';
 import AppLayout from '@/layouts/AppLayout.vue';
 import { type BreadcrumbItem } from '@/types';
 import { Room } from '@/types/app';
@@ -42,17 +43,7 @@ const props = defineProps<{
                 </main>
                 <!-- END Page Content -->
 
-                <!-- Page Footer -->
-                <footer class="fixed bottom-0 mb-3 w-full items-center border-t border-slate-200/75 bg-white px-3 lg:w-[75%]">
-                    <form class="container mx-auto flex h-20 items-center gap-2 px-4" onsubmit="return false;">
-                        <textarea
-                            type="text"
-                            class="-mx-5 block w-full rounded-lg border-0 px-5 py-4 leading-6 focus:border-indigo-500 focus:ring focus:ring-indigo-500/75"
-                            placeholder="Type a new message and hit enter.."
-                        ></textarea>
-                    </form>
-                </footer>
-                <!-- END Page Footer -->
+                <ChatFooter />
             </div>
             <!-- END Page Container -->
         </div>
