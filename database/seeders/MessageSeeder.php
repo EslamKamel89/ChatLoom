05561621d1f60971dec09dbe539ba8ed4b933e1f
EslamKamel89 <elsamkamelforex@gmail.com>
2025-04-05 09:37:57 +0200

    Add MessageSeeder to generate 1000 messages per room with user and timestamp data

--- database/seeders/MessageSeeder.php
+++ database/seeders/MessageSeeder.php
@@ -2,9 +2,11 @@
 
 namespace Database\Seeders;
 
+use App\Models\Message;
 use App\Models\Room;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Database\Eloquent\Factories\Sequence;
 use Illuminate\Database\Seeder;
 
 class MessageSeeder extends Seeder {
@@ -15,6 +17,15 @@ class MessageSeeder extends Seeder {
         /** @var Collection<int , Room> */
         $rooms = Room::all();
         $rooms->each(function (Room $room) {
+            Message::factory()->count(1000)->sequence(function (Sequence $sequence) use ($room) {
+                return [
+                    'room_id' => $room->id,
+                    'user_id' => $sequence->index % 2 == 0 ? 1 : 2,
+                    'content' => "Message {$sequence->index} in room {$room->title}",
+                    'created_at' => now()->subYear()->addMinutes($sequence->index),
+                    'updated_at' => now()->subYear()->addMinutes($sequence->index),
+                ];
+            })->create();
         });
     }
 }
