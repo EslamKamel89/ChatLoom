da01ec43c00c90d58baf2ba37e378a7b93ccb3c6
EslamKamel89 <elsamkamelforex@gmail.com>
2025-04-06 17:54:09 +0200

    Implement message retrieval in MessageController and add UserResource for user data transformation

--- app/Http/Controllers/chat/MessageController.php
+++ app/Http/Controllers/chat/MessageController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\chat;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\MessageResource;
 use App\Models\Message;
 use App\Models\Room;
 use Illuminate\Http\Request;
@@ -12,6 +13,9 @@ class MessageController extends Controller {
         if (! $request->has('room')) return [];
         $slug = $request->room;
         $room = Room::where('slug', $slug)->first();
+        return MessageResource::collection(
+            $room->messages()->with(['user'])->get()
+        );
     }
 
     // public function create() {
--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -5,7 +5,7 @@ namespace App\Http\Resources;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
-class MessageResource extends JsonResource {
+class UserResource extends JsonResource {
     /**
      * Transform the resource into an array.
      *
@@ -14,12 +14,10 @@ class MessageResource extends JsonResource {
     public function toArray(Request $request): array {
         return [
             'id' => $this->id,
-            'user_id' => $this->user_id,
-            'room_id' => $this->room_id,
-            'content' => $this->content,
+            'name' => $this->name,
+            'email' => $this->email,
             'created_at' => DateTimeResource::make($this->created_at),
             'updated_at' => DateTimeResource::make($this->updated_at),
-            'user' => UserResource::make($this->whenLoaded('user'))
         ];
     }
 }
